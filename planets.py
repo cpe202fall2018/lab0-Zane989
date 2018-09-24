9605d0a924022fe00776d96a8a123cadb1392309
Zane989 <100017245@mvla.net>
2018-09-24 09:46:15 -0700

    Updated version of planets.py.

--- planets.py
+++ planets.py
@@ -1,7 +1,10 @@
 def weight_on_planets():
-   # write your code here
-   
-   
-   
+    weight_input = int(input("What do you weigh on earth? "));
+    mars = weight_input * 0.38;
+    jupiter = weight_input * 2.34;
+
+    print("\nOn Mars you would weigh %s pounds.\nOn Jupiter you would weigh %s pounds." % (mars, jupiter));
+
+
 if __name__ == '__main__':
-   weight_on_planets()
+    weight_on_planets()
